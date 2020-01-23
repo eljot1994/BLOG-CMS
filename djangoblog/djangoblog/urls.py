38c4cd820c1f29f52f7989afb0dab0abd8f682f9
eljot1994 <jarekderda@gmail.com>
2020-01-23 15:06:41 +0100

    better template (base_layout)

--- djangoblog/djangoblog/urls.py
+++ djangoblog/djangoblog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
@@ -8,3 +9,6 @@ urlpatterns = [
     re_path(r'^articles/',include('articles.urls')),
     re_path(r'^$', views.homepage),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
