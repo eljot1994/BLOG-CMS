d3e10b843c3ffef17a51c412df082d65414cd5be
eljot1994 <jarekderda@gmail.com>
2020-01-25 22:21:27 +0100

    login (reset password)

--- djangoblog/accounts/views.py
+++ djangoblog/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 def signup_view(request):
     if request.method == 'POST':
@@ -12,11 +13,13 @@ def signup_view(request):
         form = UserCreationForm()
     return render(request,'accounts/signup.html',{'form':form})
 
+
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data = request.POST)
         if form.is_valid():
-            #log user
+            user = form.get_user()
+            login(request,user)
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
